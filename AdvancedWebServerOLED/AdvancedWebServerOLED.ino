f4731aae07511212b177b8ef43ae1a6607816bd1
Joseph Harry <findarato@gmail.com>
2016-12-22 13:26:08 -0600

    removed a lot of uneeded code and comments that just made the code look bad

--- AdvancedWebServerOLED/AdvancedWebServerOLED.ino
+++ AdvancedWebServerOLED/AdvancedWebServerOLED.ino
@@ -1,9 +1,3 @@
-// Now using ESP8266.....
-// Sample Arduino Json Web Server
-// Created by Benoit Blanchon.
-// Heavily inspired by "Web Server" from David A. Mellis and Tom Igoe
-// http://www.esp8266.com/viewtopic.php?f=29&t=7158
-
 #include <SPI.h>
 #include <Wire.h>
 #include <ArduinoJson.h>
@@ -12,24 +6,14 @@
 #include <Adafruit_SSD1306.h>
 #include "DHT.h"
 
-//////////////////////////////
-// DHT21 / AMS2301 is at GPIO2
-//////////////////////////////
+// Defines
 #define DHTPIN 14
-
-// Uncomment whatever type you're using!
-// #define DHTTYPE DHT11   // DHT 11 
-// #define DHTTYPE DHT22   // DHT 22  (AM2302)
 #define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// init DHT; 3rd parameter = 16 works for ESP8266@80MHz
-DHT dht(DHTPIN, DHTTYPE,12); 
-
 #define OLED_RESET 3
 Adafruit_SSD1306 display(OLED_RESET);
- 
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
 static const unsigned char PROGMEM logo16_glcd_bmp[] =
 { B00000000, B11000000,
   B00000001, B11000000,
@@ -47,30 +31,21 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
   B01111100, B11110000,
   B01110000, B01110000,
   B00000000, B00110000 };
- 
+
 #if (SSD1306_LCDHEIGHT != 32)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
+DHT dht(DHTPIN, DHTTYPE,12);
 // this constant won't change:
-const int  buttonPinIP = 0;    // the pin that the pushbutton is attached to
-const int  buttonPinTemp = 16;    // the pin that the pushbutton is attached to
+const int  buttonPinIP = 0;        //This will show your IP
+const int  buttonPinTemp = 16;     //This will show your Temp
 const char* ssid = "SSID";
 const char* password = "Password";
 const char* DeviceName = "Location";
 
-//Button A is #0
-//Button B is #16
-//Button C is #2
-
-
-// Variables will change:
-int buttonPushCounterIP = 0;   // counter for the number of button presses
 int buttonStateIP = LOW;         // current state of the button
 int lastButtonStateIP = HIGH;     // previous state of the button
-
-
-int buttonPushCounterTemp = 0;   // counter for the number of button presses
 int buttonStateTemp = LOW;         // current state of the button
 int lastButtonStateTemp = HIGH;     // previous state of the button
 
@@ -102,7 +77,7 @@ void connectionInfo () {
 
 
 void readTempValues() {
-   pfTemp = dht.readTemperature();   
+   pfTemp = dht.readTemperature();
    pfHum = dht.readHumidity();
    pfTempF = dht.readTemperature(true);
    float a = 17.67;
@@ -113,7 +88,7 @@ void readTempValues() {
 
   Serial.println(pfTemp);
   Serial.println(pfTempF);
-  Serial.println(pfHum);   
+  Serial.println(pfHum);
   Serial.println(pfDew);
 
 
@@ -132,9 +107,6 @@ void readTempValues() {
   display.println(pfDew);
   display.display();
 
-  
-//  Serial.println(pfTemp);
-//  Serial.println(pfTemp);
 }
 
 bool readRequest(WiFiClient& client) {
@@ -167,7 +139,7 @@ JsonObject& prepareResponse(JsonBuffer& jsonBuffer) {
   JsonArray& dewpValues = root.createNestedArray("dewpoint");
     dewpValues.add(pfDew);
   JsonArray& EsPvValues = root.createNestedArray("Systemv");
-    EsPvValues.add(pfVcc/1000,3);  
+    EsPvValues.add(pfVcc/1000,3);
     EsPvValues.add(battery/1000,3);
   return root;
 }
@@ -191,12 +163,12 @@ void setup() {
   delay(100);
 
     // Connect to WiFi network
-  WiFi.begin(ssid, password);  
+  WiFi.begin(ssid, password);
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
   delay(200);
- 
+
   // Clear the buffer.
   display.clearDisplay();
   display.println("Startup Temp IoT Thingy");
@@ -213,9 +185,9 @@ void setup() {
     display.display();
   }
 
-  
+
   dht.begin();
- 
+
   connectionInfo();
   delay(2000);
 
@@ -226,11 +198,11 @@ void setup() {
 // initialize the button pin as a input:
   pinMode(buttonPinIP, INPUT_PULLUP);
   pinMode(buttonPinTemp, INPUT_PULLUP);
-  
+
 }
 
 void loop() {
-  
+
   WiFiClient client = server.available();
   if (client) {
       bool success = readRequest(client);
@@ -244,7 +216,7 @@ void loop() {
       delay(1);
       client.stop();
   }
-  
+
 
 
 
@@ -254,14 +226,14 @@ void loop() {
   if (buttonStateIP != lastButtonStateIP) {
     // if the state has changed, increment the counter
     connectionInfo();
-  } 
+  }
 
   buttonStateTemp = digitalRead(buttonPinTemp);
    // compare the buttonState to its previous state
   if (buttonStateTemp != lastButtonStateTemp) {
     // if the state has changed, increment the counter
     readTempValues();
-  } 
+  }
 
 
 }
